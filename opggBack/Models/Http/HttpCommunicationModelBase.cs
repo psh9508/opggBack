7e9faae9bd3ea4b0425d7c68be152f295cea7001
Masuri <psh0258@gmail.com>
2022-10-06 00:26:18 +0900

    Http 통신 기본 모델 추가

---
+++ opggBack/Models/Http/HttpCommunicationModelBase.cs
@@ -0,0 +1,8 @@
+﻿namespace opggBack.Models.Http
+{
+    public class HttpCommunicationModelBase
+    {
+        //public string Message { get; set; } = string.Empty;
+        public Exception Exception { get; set; }
+    }
+}
